521c8186da3c4a909bcffe73af3cc742390ba748
certainly1182 <fawcettjames63@gmail.com>
2024-12-12 13:10:33 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Lascells-WebPlot/sw.js', { scope: '/Lascells-WebPlot/' })})}
